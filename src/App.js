7a45fbbe83645238b29e71ee04dcecb605a1f729
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2021-05-21 17:47:32 -0500

    Dark mode implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React, { useState } from "react";
 import "./sass/styles.scss";
 import Header from "./components/Header";
 import Comic from "./components/Comic";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const handleDarkMode = () => {
+    const body = document.body.style;
+
+    if (darkMode) {
+      body.backgroundColor = "white";
+      body.color = "black";
+    } else {
+      body.backgroundColor = "#121212";
+      body.color = "white";
+    }
+
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div className="gridContainer">
-      <Header />
+      <Header darkMode={darkMode} handleDarkMode={handleDarkMode} />
       <Comic />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import "../sass/header.scss";
 
-const Header = () => {
-  const [darkMode, setDarkMode] = useState(true);
-
-  const handleDarkMode = () => setDarkMode(!darkMode);
-
+const Header = (props) => {
   return (
     <div className="header">
       <h1 className="header__title">XKXD Ramdom Comic</h1>
-      <button className="header__button" type="button" onClick={handleDarkMode}>
-        {darkMode ? "Light Mode" : "Dark Mode"}
+      <button
+        className="header__button"
+        type="button"
+        onClick={props.handleDarkMode}
+      >
+        {props.darkMode ? "Light Mode" : "Dark Mode"}
       </button>
     </div>
   );
